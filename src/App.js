ce5071067e72b64f488475546b8cf5d60b7abda0
agsafronenko <109076625+agsafronenko@users.noreply.github.com>
2022-10-21 08:12:21 +0200

    adding primary files for the future project

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./styles/styles.css";
+import $ from "jquery";
+
+export default class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <>
+        <Footer />
+      </>
+    );
+  }
+}
+
+function Footer() {
+  return (
+    <footer>
+      <div id="footer">This project was build using: HTML, CSS, JavaScript, React, Redux, jQuery, Bootstrap and SASS</div>
+    </footer>
+  );
+}
